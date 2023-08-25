f09dfa436d97d861276467480a0a56b31d8b42ed
pilu9147 <priyankabhesaniya617@gmail.com>
2023-08-25 12:43:40 +0530

    first

---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import AccountCircle from './AccountCircle';
+import image from '../image/typing-img.jpg';
+
+const Header = ()=>{
+   
+    return(
+        <div className="header">
+            <div className="logo">
+                <img className='logoo' src={image}/><span>TypOmeTer</span>
+            </div>
+        
+            <div className="user-icon">
+                <AccountCircle size="large"/>
+                {/*user-icon here*/}
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
